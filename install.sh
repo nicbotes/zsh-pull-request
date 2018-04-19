a9b8d2163c8bf7521dfabf0b19484eeabd4b1168
Nicholas Botes <nic@Nicholass-MacBook-Pro.local>
2018-04-19 16:18:29 +0200

    adds installation file

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 zshcompletions=~/.oh-my-zsh/completions
 zshfunctions=~/.oh-my-zsh/functions
@@ -6,7 +6,22 @@ zshfunctions=~/.oh-my-zsh/functions
 mkdir -p $zshcompletions
 mkdir -p $zshfunctions
 
-ln -s `pwd`/pr $zshfunctions/pr
-ln -s `pwd`/_pr $zshcompletions/_pr
+source=git@github.com:nicbotes/zsh-pull-request.git
+destination=zsh-pull-request
 
-echo "source ~/.oh-my-zsh/functions/pr" >> ~/.zshrc
+cd /tmp
+git clone $source $destination
+
+cd $destination
+
+cp pr $zshfunctions
+cp _pr $zshcompletions
+
+cd /tmp
+rm -rf $destination
+
+echo "installed.\n"
+echo "please run the following and open a new terminal"
+echo "echo \"source ~/.oh-my-zsh/functions/pr\" >> ~/.zshrc"
+echo "please use"
+echo "$ pr <base-branch>"
